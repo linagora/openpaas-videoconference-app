a6550bdae2971b3876ef7be4600b25ef02244c54
Christophe Hamerling <chamerling@linagora.com>
2018-10-26 14:32:28 +0200

    Clean Vue generated files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.svg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your OpenPaaS Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
