c8696c7aebd3e4758c2eac6523b8e03c5548f9ad
Loic AUBLET <laublet@linagora.com>
2018-11-26 16:01:51 +0100

    #21 added vue-i18n

---
+++ src/i18n/index.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+import { FALLBACK_LANGUAGE, DEFAULT_LANGUAGE } from "@/i18n/constants";
+import en from "@/i18n/lang/en.json";
+import fr from "@/i18n/lang/fr.json";
+import vi from "@/i18n/lang/vi.json";
+
+Vue.use(VueI18n);
+
+const browserLocale = navigator.language;
+const locale = browserLocale.includes("-") ? browserLocale.split("-")[0] : browserLocale || DEFAULT_LANGUAGE;
+
+export const i18n = new VueI18n({
+  locale: locale,
+  fallbackLocale: FALLBACK_LANGUAGE,
+  messages: { fr, en, vi }
+});
