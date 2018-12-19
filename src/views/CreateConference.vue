68189926ad9b1bd7c24aa2efc67b55de0ba247cf
Christophe HAMERLING <chamerling@linagora.com>
2018-12-19 16:42:00 +0100

    #15 Format template

--- src/views/CreateConference.vue
+++ src/views/CreateConference.vue
@@ -1,29 +1,29 @@
 <template>
-    <v-container fluid fill-height>
-      <v-layout align-center justify-center>
-          <v-card width="60vw" class="elevation-12">
-            <v-card-title class="white--text headline primary lighten-2 justify-center">
-              {{$t('Create a new conference')}}
-            </v-card-title>
-            <v-card-text>
-              <v-form ref="form" v-model="valid" @keydown.native.enter="submit"  onSubmit="return false;">
-                <v-text-field
-                  v-model="conferenceName"
-                  :rules="conferenceNameRules"
-                  :label="$t('Enter your conference name')"
-                  type="text"
-                  autofocus
-                  required
-                ></v-text-field>
-                <v-btn
-                  :disabled="!valid"
-                  @click="submit"
-                >{{$t('Create')}}</v-btn>
-              </v-form>
-            </v-card-text>
-          </v-card>
-      </v-layout>
-    </v-container>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-card width="60vw" class="elevation-12">
+        <v-card-title class="white--text headline primary lighten-2 justify-center">
+          {{$t('Create a new conference')}}
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" @keydown.native.enter="submit"  onSubmit="return false;">
+            <v-text-field
+              v-model="conferenceName"
+              :rules="conferenceNameRules"
+              :label="$t('Enter your conference name')"
+              type="text"
+              autofocus
+              required
+            ></v-text-field>
+            <v-btn
+              :disabled="!valid"
+              @click="submit"
+            >{{$t('Create')}}</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
