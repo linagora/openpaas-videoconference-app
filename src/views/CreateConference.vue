870130877ad71d83ec9f0af1c839eae366fd6113
Christophe HAMERLING <chamerling@linagora.com>
2018-12-19 16:58:15 +0100

    #15 use the form style everywhere

--- src/views/CreateConference.vue
+++ src/views/CreateConference.vue
@@ -1,27 +1,32 @@
 <template>
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
-      <v-card width="60vw" class="elevation-12">
-        <v-card-title class="white--text headline primary lighten-2 justify-center">
-          {{$t('Create a new conference')}}
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" v-model="valid" @keydown.native.enter="submit"  onSubmit="return false;">
-            <v-text-field
-              v-model="conferenceName"
-              :rules="conferenceNameRules"
-              :label="$t('Enter your conference name')"
-              type="text"
-              autofocus
-              required
-            ></v-text-field>
-            <v-btn
-              :disabled="!valid"
-              @click="submit"
-            >{{$t('Create')}}</v-btn>
-          </v-form>
-        </v-card-text>
-      </v-card>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar color="primary">
+            <v-toolbar-title class="white--text">{{$t('Create a new conference')}}</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" @keydown.native.enter="submit"  onSubmit="return false;">
+              <v-text-field
+                v-model="conferenceName"
+                :rules="conferenceNameRules"
+                :label="$t('Enter your conference name')"
+                type="text"
+                autofocus
+                required
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+              <v-btn
+                :disabled="!valid"
+                @click="submit"
+              >{{$t('Create')}}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
