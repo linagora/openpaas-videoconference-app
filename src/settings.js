6aa0c501ff2826f2fdb048587f41852ae5a53f27
Christophe HAMERLING <chamerling@linagora.com>
2018-12-20 11:11:50 +0100

    #20 Default URL is the .env one

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 const defaults = {
-  OPENPAAS_URL: "http://localhost:8080"
+  OPENPAAS_URL: process.env.VUE_APP_OPENPAAS_URL || "http://localhost:8080"
 };
 
 const settings = { ...defaults, ...window.openpaas };
